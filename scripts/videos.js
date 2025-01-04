d415d59e856e12477e30292a69ecb89126499417
rajeshbd99 <rajesh99.bd@gmail.com>
2025-01-04 16:33:48 +0600

    add handling for empty video categories with user-friendly message

--- scripts/videos.js
+++ scripts/videos.js
@@ -40,6 +40,20 @@ const loadCategoryVideos = (id) => {
 const displayVideos = (videos) => {
   const videoContainer = document.getElementById("videos");
   videoContainer.innerHTML = "";
+  
+  if(videos.length === 0){
+    videoContainer.classList.remove("grid");
+    videoContainer.innerHTML = `
+    <div class= " min-h-[400px] flex flex-col gap-5 justify-center items-center"><img src="assets/Icon.png"/>
+    <h2 class="text-2xl font-bold">No Content Here In This Category</h2>
+    </div>
+    `;
+    return;
+  }
+  else{
+    videoContainer.classList.add("grid");
+  }
+
   videos.forEach((video) => {
     console.log(video);
     const card = document.createElement("div");
