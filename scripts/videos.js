8f21965e46bfafca1767ca2283ce63a577fe6081
rajeshbd99 <rajesh99.bd@gmail.com>
2025-01-04 17:30:59 +0600

    add search functionality to load videos based on user input

--- scripts/videos.js
+++ scripts/videos.js
@@ -11,8 +11,10 @@ const loadCategories = () => {
 };
 
 //create loadVideos function
-const loadVideos = () => {
-  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+const loadVideos = (searchText = "") => {
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`
+
+  ) 
     .then((res) => res.json())
     .then((data) => displayVideos(data.videos))
     .catch((error) => console.log(error));
@@ -146,6 +148,10 @@ const displayCategories = (categories) => {
     categoryContainer.appendChild(buttonContainer);
   });
 };
+
+document.getElementById("search-input").addEventListener("keyup", (e) => {
+    loadVideos(e.target.value);
+});
 
 loadCategories();
 loadVideos();
