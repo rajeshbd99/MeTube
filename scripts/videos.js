92ec382339a5c1e75b64be061fbee2c2a1006048
rajeshbd99 <rajesh99.bd@gmail.com>
2025-01-04 16:17:36 +0600

    add functionality to load videos by category and improve posted date display

--- scripts/videos.js
+++ scripts/videos.js
@@ -28,10 +28,18 @@ function getTimeString(time) {
     return `${hour}h ${minute}m ${remainingSeconds}s ago`;
 }
 
+const loadCategoryVideos = (id) => {  
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.category))
+    .catch((error) => console.log(error));
+   };
+
 //create displayVideos function
 
 const displayVideos = (videos) => {
   const videoContainer = document.getElementById("videos");
+  videoContainer.innerHTML = "";
   videos.forEach((video) => {
     console.log(video);
     const card = document.createElement("div");
@@ -40,7 +48,7 @@ const displayVideos = (videos) => {
         <figure class= "h-[200px] relative">
             <img src=${video.thumbnail} 
             class = " h-full w-full object-cover " alt="Shoes" />
-            ${video.others.posted_date?.length ==0 ? "" : `<span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${getTimeString(video.others.posted_date)} </span>`}
+            ${video.others.posted_date?.length ==0 ? "" : `<span class="absolute text-xs right-2 bottom-2 bg-black text-white rounded p-1">${getTimeString(video.others.posted_date)} </span>`}
         </figure>
         <div class="px-0 py-2 flex gap-2">
             <div>
@@ -70,12 +78,16 @@ const displayCategories = (categories) => {
   categories.forEach((item) => {
     console.log(item);
     //create a button
-    const button = document.createElement("button");
-    button.classList = "btn btn-primary m-2";
-    button.innerText = item.category;
+    const buttonContainer = document.createElement("div");
+    buttonContainer.innerHTML= `
+    <button onclick="loadCategoryVideos(${item.category_id})" class="btn">
+    ${item.category}
+    </button>
+    `
+    
 
     //add button to category container
-    categoryContainer.appendChild(button);
+    categoryContainer.appendChild(buttonContainer);
   });
 };
 
